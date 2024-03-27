18a1834a44184f60cb97a01c3496f5462d54fcae
Trần Thị Hường <dhhuongdhlt1@gmail.com>
2024-03-27 13:31:36 +0700

    A

--- javaLop/src/exp/myFrame.java
+++ javaLop/src/exp/myFrame.java
@@ -1,3 +1,15 @@
-package exp;
-
-
+interface In1 {
+    int a = 10;
+    void display();
+}
+class TestClass implements In1 {
+    public void display(){
+      System.out.println("Geek");
+    }
+    public static void main(String[] args)
+    {
+//        TestClass t = new TestClass();
+//        t.display(); 
+        System.out.println(a);
+    }
+}
